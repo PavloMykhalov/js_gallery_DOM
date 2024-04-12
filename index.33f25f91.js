18d7a58c209aba63057c9bed674b19dec83cc38a
Pavlo Mykhalov <pavlomihaliov@gmail.com>
2024-04-12 16:36:32 +0300

    make build

---
+++ index.33f25f91.js
@@ -0,0 +1,2 @@
+var e=document.querySelector("div.gallery"),t=document.getElementById("largeImg");e.addEventListener("click",function(e){e.preventDefault();var r=("IMG"===e.target.tagName?e.target.parentNode:e.target).getAttribute("href");t.setAttribute("src",r)});
+//# sourceMappingURL=index.33f25f91.js.map
---
+++ index.f75de5e1.js
@@ -0,0 +1,11 @@
+"use strict";
+const gallery = document.querySelector("div.gallery");
+const largeImg = document.getElementById("largeImg");
+gallery.addEventListener("click", (e)=>{
+    e.preventDefault();
+    const target = e.target.tagName === "IMG" ? e.target.parentNode : e.target;
+    const href = target.getAttribute("href");
+    largeImg.setAttribute("src", href);
+});
+
+//# sourceMappingURL=index.f75de5e1.js.map
